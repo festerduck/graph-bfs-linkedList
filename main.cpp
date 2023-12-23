e654bcebd9b73c3fb1e211e85cca14b4cca449aa
Abdul Rehman <festerduck@gmail.com>
2023-12-23 19:05:07 +0500

    Modified for the Lab using Mapped Names

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <queue>
+
+#define VERTICES 15
 using namespace std; 
 struct Node{
   int key;
@@ -53,7 +55,9 @@ void Graph::printGraph() const{
     }
     Node* current = array[i]->head;
     while (current != NULL) {
-      cout << current->location << " ";
+      if(current->next != NULL)
+        cout << current->location << ", ";
+      else cout << current->location;
       current = current->next;
     }
     cout << endl;
@@ -88,7 +92,7 @@ void Graph::BFS(int source)
   Q.push(source);
   while (!Q.empty()) {
     source = Q.front();
-    cout << array[source]->location << " ";
+    cout << array[source]->location << ", ";
     Q.pop();
     Node* current = array[source]->head;
     while (current != NULL) {
@@ -101,37 +105,88 @@ void Graph::BFS(int source)
   }
 
 }
-
+/*
+ * PIEAS = 0
+ * Chirrah = 1
+ * Thanda Pani = 2
+ * Ali Pur = 3
+ * Mohara = 4
+ * Taramri = 5
+ * Chatta Bakhtawar = 6
+ * Kuri = 7
+ * Chak Shahzad = 8
+ * Bani Gala = 9
+ * Bara Kahu = 10
+ * Athal = 11
+ * Malot = 12
+ * Jagiot = 13
+ * Pind Begwal = 14
+ * */
 int main()
 {
-  string locations[] = {"Pieas", "LakeView", "Thanda Paani", "Nilore", "Chirrah"};
-  cout << "Graph" << endl;
-  Graph g1(5);
-  for(int i = 0; i < 5; i++){
+  string locations[] = {
+    "PIEAS",
+    "Chirrah",
+    "Thanda Pani",
+    "Ali Pur",
+    "Mohara",
+    "Taramri",
+    "Chatta Bakhtawar",
+    "Kuri",
+    "Chak Shahzad",
+    "Bani Gala",
+    "Bara Kahu",
+    "Athal",
+    "Malot",
+    "Jagiot",
+    "Pind Begwal"
+  };
+  Graph g1(VERTICES);
+  for(int i = 0; i < VERTICES; i++){
    g1.addVertex(i, locations[i]);
   }
   g1.addEdge(0, 1);
+  g1.addEdge(0, 2);
   g1.addEdge(0, 4);
-  g1.addEdge(0, 3);
-  g1.addEdge(1, 2);
-  g1.addEdge(3, 0);
-  g1.addEdge(3, 4);
+  g1.addEdge(1, 0);
+  g1.addEdge(2, 0);
+  g1.addEdge(2, 3);
+  g1.addEdge(3, 2);
+  g1.addEdge(3, 5);
+  g1.addEdge(3, 13);
   g1.addEdge(4, 0);
-  g1.addEdge(4, 3);
+  g1.addEdge(4, 14);
+  g1.addEdge(5, 3);
+  g1.addEdge(5, 6);
+  g1.addEdge(6, 7);
+  g1.addEdge(6, 8);
+  g1.addEdge(6, 5);
+  g1.addEdge(7, 6);
+  g1.addEdge(7, 8);
+  g1.addEdge(7, 12);
+  g1.addEdge(8, 6);
+  g1.addEdge(8, 7);
+  g1.addEdge(8, 9);
+  g1.addEdge(9, 8);
+  g1.addEdge(9, 10);
+  g1.addEdge(10, 9);
+  g1.addEdge(10, 11);
+  g1.addEdge(11, 10);
+  g1.addEdge(11, 12);
+  g1.addEdge(11, 14);
+  g1.addEdge(12, 11);
+  g1.addEdge(12, 7);
+  g1.addEdge(12, 13);
+  g1.addEdge(13, 12);
+  g1.addEdge(13, 3);
+  g1.addEdge(13, 14);
+  g1.addEdge(14, 13);
+  g1.addEdge(14, 11);
+  g1.addEdge(14, 4);
   g1.printGraph();
-  Graph g(5);
-  for (int i = 0; i < 5; i++) {
-    g.addVertex(i, locations[i]);
-  }
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
- for (int i = 0; i < 4; i++) {
+ for (int i = 0; i < VERTICES; i++) {
     cout << "BFS with source " << locations[i] << endl;
-    g.BFS(i);
+    g1.BFS(i);
     cout << endl;
   }
   return 0;
